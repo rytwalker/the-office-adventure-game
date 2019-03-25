a237e2933ceca7a8d7c73e3d12d302a6214c7b17
Ryan Walker <rytwalker@gmail.com>
2019-03-25 19:23:09 -0400

    minor updates

--- src/data/text.py
+++ src/data/text.py
@@ -1,5 +1,7 @@
 text = {
-    'intro': "Welcome to Scranton Buisness Park. Home of the best regional branch of Dunder Mifflin Paper Company in the world! We are glad to have you, uh what is your name?",
+    'intro': """Welcome to Scranton Buisness Park.
+Home of the best regional branch of Dunder Mifflin Paper Company in the world!
+We are glad to have you, uh what is your name?""",
     'help': """Commands:
     - Move: "n", "s", "w", "e"
     - Pick up item: "get <item name>" or "take <item name>"
@@ -7,6 +9,5 @@ text = {
     - Show inventory: "inventory" or "i"
     - Inspect: "inspect <item name>"
     - Quit: "q"
-
-        """
+    """
 }
